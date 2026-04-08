a0e026e7416ff7eeb3ec25c279c723a3075eabba
FreddyDihver <149085695+FreddyDihver@users.noreply.github.com>
2026-04-08 18:49:44 +0200

    All imports

---
+++ SentenceFunctions.py
@@ -0,0 +1,12 @@
+# Import custom functions
+from LoadingFunctions import *
+from StructureFunctions import *
+from TrainingFunctions import *
+from PostProcessFunctions import *
+from PrintFunctions import *
+
+# Import custom class
+from OnlineContrastiveLossMean import *
+
+
+
